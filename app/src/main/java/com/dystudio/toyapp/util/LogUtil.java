5274a72ee22c7c214ca1544f09e7f4642fade5ed
aipoka <aipoka@163.com>
2017-09-26 11:20:44 +0800

    pizza startup with Pizza0 as tag

---
+++ app/src/main/java/com/dystudio/toyapp/util/LogUtil.java
@@ -0,0 +1,34 @@
+package com.dystudio.toyapp.util;
+
+
+import android.util.Log;
+
+public class LogUtil {
+    private static LogUtil sLogUtil;
+    private String mTag = "com.dystudio";
+    private boolean mDebugFlag = true;
+
+    private LogUtil(){ }
+
+    public static LogUtil getLogger( ){
+        if (sLogUtil == null) {
+            sLogUtil = new LogUtil();
+        }
+        return sLogUtil;
+    }
+
+    public LogUtil setTag(String tag) {
+        mTag = tag;
+        return this;
+    }
+
+    public LogUtil setDebugFlag(boolean debugFlag) {
+        mDebugFlag = debugFlag;
+        return this;
+    }
+
+    public void log(String msg){
+        if(mDebugFlag)
+            Log.d(mTag,msg);
+    }
+}
